7d792773616c79401ca8ed1a799bb10e2a19747c
Ana <135903958+analeopoldino@users.noreply.github.com>
2023-07-31 19:11:29 -0300

    Atualizacao

--- javascript/script.js
+++ javascript/script.js
@@ -1,135 +1,114 @@
-// Aqui são declaradas as constantes e variáveis para os elementos do DOM
 let elementoResultado = document.querySelector(".js-resultado");
 let botoesPadroes = document.querySelectorAll(".js-btn-padroes");
 let botaoResultado = document.querySelector(".js-btn-igual");
 let botaoAC = document.querySelector(".js-btn-ac");
-let botaMaisMenos = document.querySelector(".js-btn-mais-menos");
-let botaoDelete = document.querySelector(".js-btn-del");
-let currentOperationText = document.querySelector("#current-operation");
-
-// Aqui está a classe da calculadora
-class Calculator {
-    constructor() {
-        this.currentOperation = "";
-    }
-
-    addDigit(digit) {
-        if (digit === "." && this.currentOperation.includes(".")) {
-            return;
-        }
-
-        const isOperator = ["+", "-", "*", "/"].includes(digit);
-
-        if (isOperator && this.currentOperation.length > 1 && isNaN(this.currentOperation[this.currentOperation.length - 2])) {
-            this.currentOperation = this.currentOperation.slice(0, -2) + ` ${digit} `;
-        } else {
-            this.currentOperation += digit;
-        }
-
-        this.updateScreen();
-    }
-
-    updateScreen() {
-        currentOperationText.innerText = this.currentOperation;
-    }
-
-    processEqualOperator() {
-        let operation = this.currentOperation;
-        let result = eval(operation);
-        this.currentOperation = result.toString();
-        this.updateScreen();
-    }
-
-    processClearOperator() {
-        this.currentOperation = "";
-        this.updateScreen();
-    }
-
-    processDelOperator() {
-        this.currentOperation = this.currentOperation.slice(0, -1);
-        this.updateScreen();
-    }
-}
-
-// Instância da calculadora
-const calculadora = new Calculator();
+let botaoMaisMenos = document.querySelector(".js-btn-mais-menos");
+let botaoDeletar = document.querySelector(".js-btn-del");
 
 function adicionarElementoAoInputResultado(numeroDigitado) {
     verificarSimboloDuplicado(numeroDigitado);
-
-    const isOperator = ["+", "-", "*", "/"].includes(numeroDigitado);
-    if (isOperator) {
-        elementoResultado.value = " ";
-    } else if (verificarSimboloInicial(numeroDigitado)) {
-        return;
-    }
-
-    if (!isOperator) {
-        if (elementoResultado.value === "0") {
-            elementoResultado.value = numeroDigitado;
-        } else {
-            elementoResultado.value += numeroDigitado;
-        }
-    }
-    calculadora.addDigit(numeroDigitado);
+    if (verificarSimboloInicial(numeroDigitado)) return;
+    elementoResultado.value += numeroDigitado;
 }
 
 function executarCalculo() {
     try {
-        calculadora.processEqualOperator();
-        elementoResultado.value = calculadora.currentOperation;
+        const resultado = calcularExpressao(elementoResultado.value);
+        elementoResultado.value = resultado;
     } catch {
-        alert("Algo deu errado. Tente novamente");
+        alert("Algo deu errado. Tente novamente.");
     }
 }
 
 function limparResultado() {
-    elementoResultado.value = "0";
-    calculadora.processClearOperator();
+    elementoResultado.value = "";
 }
 
 function trocarSinalDaConta() {
-    if (Number(elementoResultado.value)) {
-        elementoResultado.value = elementoResultado * -1;
-    }
-}
-
-function deletarUltimaValorDoResultado() {
-    elementoResultado.value = elementoResultado.value.slice(0, -1);
-    calculadora.processDelOperator();
-}
-
-function verificarSimboloDuplicado(numeroDigitadoRecebidoPorParametro) {
-  const isOperator = ["+", "-", "*", "/"].includes(numeroDigitadoRecebidoPorParametro);
-
-  if (isOperator) {
-    const ultimoCaractere = elementoResultado.value.slice(-1);
-
-    if (["+", "-", "*", "/"].includes(ultimoCaractere)) {
-      // Se o último caractere no input é um operador,
-      // substitua o último pelo novo
-      elementoResultado.value = elementoResultado.value.slice(0, -1) + numeroDigitadoRecebidoPorParametro;
-      return;
-    }
+  const valor = elementoResultado.value;
+  if (Number(valor)) {
+    elementoResultado.value = valor.startsWith("-")
+      ? valor.substring(1)
+      : `-${valor}`;
   }
 }
 
-function verificarSimboloInicial(numeroDigitadoRecebidoPorParametro) {
-    return elementoResultado.value.length == 0 && !Number(numeroDigitadoRecebidoPorParametro);
+function deletarUltimaLetraDoResultado() {
+    elementoResultado.value = elementoResultado.value.slice(0, -1);
 }
+
+function verificarSimboloDuplicado(numeroDigitadoRecebidoPorParametro) {
+    let ultimoValorNoInputResultado =
+        elementoResultado.value[elementoResultado.value.length - 1];
+    if (
+        ultimoValorNoInputResultado &&
+        !Number(ultimoValorNoInputResultado) &&
+        !Number(numeroDigitadoRecebidoPorParametro) &&
+        ultimoValorNoInputResultado != 0 &&
+        numeroDigitadoRecebidoPorParametro != 0
+    ) {
+        deletarUltimaLetraDoResultado();
+    }
+}
+
+function verificarSimboloInicial(numeroDigitadoRecebidoPorParametro) {
+    if (
+        elementoResultado.value.length == 0 &&
+        !Number(numeroDigitadoRecebidoPorParametro)
+    ) {
+        return true;
+    }
+}
+
+function calcularExpressao(expressao) {
+  const regex = /([+\-]?\d+\.?\d*)\s*([+\-*/])\s*([+\-]?\d+\.?\d*)/;
+  const match = expressao.match(regex);
+  if (!match) {
+    throw new Error("Expressão inválida");
+  }
+
+  const operando1 = parseFloat(match[1]);
+  const operador = match[2];
+  const operando2 = parseFloat(match[3]);
+
+  switch (operador) {
+    case "+":
+      return operando1 + operando2;
+    case "-":
+      return operando1 - operando2;
+    case "*":
+      return operando1 * operando2;
+    case "/":
+      return operando1 / operando2;
+    default:
+      throw new Error("Operador inválido");
+  }
+}
+
 
 function gerenciarEscutadores() {
     botoesPadroes.forEach((elementoCorrente) => {
         elementoCorrente.addEventListener("click", () => {
             let valorDoElementoClicado = elementoCorrente.dataset.valor;
-            adicionarElementoAoInputResultado(valorDoElementoClicado)
+            adicionarElementoAoInputResultado(valorDoElementoClicado);
         });
     });
 
-    botaoResultado.addEventListener('click', executarCalculo);
-    botaoAC.addEventListener('click', limparResultado);
-    botaMaisMenos.addEventListener('click', trocarSinalDaConta);
-    botaoDelete.addEventListener('click', deletarUltimaValorDoResultado);
+    botaoResultado.addEventListener("click", () => {
+        executarCalculo();
+    });
+
+    botaoAC.addEventListener("click", () => {
+        limparResultado();
+    });
+
+    botaoMaisMenos.addEventListener("click", () => {
+        trocarSinalDaConta();
+    });
+
+    botaoDeletar.addEventListener("click", () => {
+        deletarUltimaLetraDoResultado();
+    });
 }
 
 gerenciarEscutadores();
